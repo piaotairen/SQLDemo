cef189fff6ab6e841cad0aa3027e4fb8c96cb538
qianfeng <qianfeng@XCode.local>
2013-05-23 09:08:06 +0800

    Initial Commit

---
+++ mySQL/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  mySQL
+//
+//  Created by qianfeng on 13-5-23.
+//  Copyright (c) 2013年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ mySQL/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  mySQL
+//
+//  Created by qianfeng on 13-5-23.
+//  Copyright (c) 2013年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
